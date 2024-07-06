385414d7fac45cfbedd962978762c21ccbf14b85
buğra uysal <uysalbugra134@gmail.com>
2024-07-06 16:42:41 +0300

    Düzenlendi ve yüzde hesaplama formülü eklendi

--- yzdhsp.py
+++ yzdhsp.py
@@ -1,14 +1,13 @@
-import numpy
 
 
 
 """ kullanıcıdan alınana 1. sayı """
 klncal=float(input("sayı giriniz"))
-""" kullanıcıdan alınana 2. sayı """
-klncalıkı=float(input("sayı giriniz"))
 
-""" alınan sayıların [10,20] gibi gözükmesi saağlanır """
-alınanavrı=klncal ,klncalıkı
-""" yüzdesi hesaplanır """
-bb=numpy.percentile(alınanavrı,50)
-print(bb)
+""" kullanıcıdan alınana yüzde oranı """
+klncalıkı=float(input("yüzde oranı giriniz"))
+
+""" yüzde formülü """
+yzdm=(klncal/100)*klncalıkı
+
+print(yzdm)
